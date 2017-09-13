aff7e49ae1a0510aee0043c78b469f9c4b4bf69e
Nick Clifford <nick@nickclifford.me>
2017-09-13 10:53:44 -0500

    Sass handler, Haml -> Slang, partials

--- index.cr
+++ index.cr
@@ -1,8 +1,18 @@
 require "kemal"
+require "kilt/slang"
+require "yaml"
 require "./handlers/sass_handler"
 
-def partial(id : Symbol, title : String)
-  # TODO
+macro partial(id, title)
+  config_path = "./config/{{id.id}}.yaml"
+  config = YAML.parse(File.read(config_path))
+
+  id = {{id.stringify}}
+  title = {{title}}
+  stylesheet = %{<link href="/css/{{id.id}}.css" rel="stylesheet">} if p(File.exists?("./scss/{{id.id}}.scss"))
+
+  # TODO: fix Slang to allow unescaped interpolation
+  HTML.unescape(render "./views/{{id.id}}.slang", "./views/layout.slang")
 end
 
 get "/" do
