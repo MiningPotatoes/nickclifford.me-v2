ee086c53e5949805aa8892d2385d5850aab7a8be
Nick Clifford <nick@nickclifford.me>
2017-09-20 20:47:32 -0500

    i think i have it working but i want to try to hide the deploydir

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,7 +5,7 @@ chmod 600 .travis/id_rsa
 ssh-add .travis/id_rsa
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT/$DEPLOY_DIR
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 ssh apps@$IP -p $PORT <<EOF
