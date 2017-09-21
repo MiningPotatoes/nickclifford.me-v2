64c656df6c0b4944dd6d3387c2b46633b4f9206c
Nick Clifford <nick@nickclifford.me>
2017-09-20 20:59:50 -0500

    remove echo

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,8 +8,6 @@ git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
-echo "SSHing into apps"
-
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   crystal build --release --no-debug index.cr
