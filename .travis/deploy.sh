7134e0f74512cc2dace6e0a73c0da834955b36aa
Nick Clifford <nick@nickclifford.me>
2017-09-20 19:51:07 -0500

    let's try with Travis config

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,9 +2,10 @@
 
 eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
-if [ -z 'ssh-keygen -F $IP' ]; then
-  ssh-keyscan -H -p $PORT "[$IP]" >> ~/.ssh/known_hosts
-fi
+
+# if [ -z 'ssh-keygen -F $IP' ]; then
+#   ssh-keyscan -H -p $PORT "[$IP]" >> ~/.ssh/known_hosts
+# fi
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
