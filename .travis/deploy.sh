41c298c5dd456b9465870d711e22cf3a3d1d2515
Nick Clifford <nick@nickclifford.me>
2017-09-20 20:01:24 -0500

    git remote

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,12 +3,8 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 
-# if [ -z 'ssh-keygen -F $IP' ]; then
-#   ssh-keyscan -H -p $PORT "[$IP]" >> ~/.ssh/known_hosts
-# fi
-
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://git@$IP:$PORT/$DEPLOY_DIR
 git push deploy master
 
 ssh apps@$IP -p $PORT << EOF
