4f237d177c83f5ad24a5a5d391384fbb13a5cc40
Nick Clifford <nick@nickclifford.me>
2017-09-20 19:02:14 -0500

    deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)"
-chmod 660 .travis/id_rsa
+chmod 600 .travis/id_rsa
 if [ -z 'ssh-keygen -F $IP' ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
@@ -10,7 +10,7 @@ git config --global push.default matching
 git remote add deploy git@$IP:$DEPLOY_DIR
 git push deploy master
 
-ssh apps@$IP << EOF
+ssh apps@$IP -p $PORT << EOF
   cd $DEPLOY_DIR
   crystal build --release index.cr
 EOF
