2f66873a802741bef57c7047b48cb27cba63ce1b
Nick Clifford <nick@nickclifford.me>
2017-09-20 19:09:18 -0500

    i hate this

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,11 +3,11 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 if [ -z 'ssh-keygen -F $IP' ]; then
-  ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+  ssh-keyscan -H -p $PORT $IP >> ~/.ssh/known_hosts
 fi
 
 git config --global push.default matching
-git remote add deploy git@$IP:$DEPLOY_DIR
+git remote add deploy git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 ssh apps@$IP -p $PORT << EOF
