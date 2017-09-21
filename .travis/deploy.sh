921692324048e53eda5ce2fd38af0c2182d29052
Nick Clifford <nick@nickclifford.me>
2017-09-20 20:32:14 -0500

    heredoc

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,7 @@ git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT/$DEPLOY_DIR
 git push deploy master
 
-ssh apps@$IP -p $PORT << EOF
+ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   crystal build --release index.cr
 EOF
