e72cf1aafe365e63988d9d725f3f3cc6ddeeec54
Nick Clifford <nick@nickclifford.me>
2017-09-20 19:19:31 -0500

    known hosts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 if [ -z 'ssh-keygen -F $IP' ]; then
-  ssh-keyscan -H -p $PORT $IP >> ~/.ssh/known_hosts
+  ssh-keyscan -H "[$IP]:$PORT" >> ~/.ssh/known_hosts
 fi
 
 git config --global push.default matching
