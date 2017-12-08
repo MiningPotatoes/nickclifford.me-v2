3435cfe81f849458a61ce809d7450285c18d9595
Nick Clifford <nick@nickclifford.me>
2017-12-07 18:34:47 -0600

    add some debugging info

--- index.cr
+++ index.cr
@@ -27,28 +27,28 @@ get "/" do
   partial :index, "Home"
 end
 
-# get "/advisory" do
-#   advisees = %w(Ben Eli Gini Hadley Nick Palmer Sameer Sloane Skyler)
-#
-#   rotation = JSON.parse(HTTP::Client.post("https://api.mymicds.net/portal/day-rotation").body)
-#   err = rotation["error"].as_s?
-#
-#   days = rotation["days"].as_h?
-#
-#   unless days.nil?
-#     days = days.each_with_object({} of Time => Int32) do |(year, v0), memo|
-#       v0.as(Hash(String, JSON::Type)).each do |(month, v1)|
-#         v1.as(Hash(String, JSON::Type)).each do |(day, v2)|
-#           cycle_day = JSON::Any.new(v2).as_i
-#           memo[Time.new(year.to_i, month.to_i, day.to_i)] = cycle_day unless cycle_day == 1 || cycle_day == 4
-#         end
-#       end
-#     end
-#
-#     day_pairs = advisees.cycle.zip(days.each)
-#   end
-#
-#   partial :advisory, "Advisory Snack"
-# end
+get "/advisory" do
+  advisees = %w(Ben Eli Gini Hadley Nick Palmer Sameer Sloane Skyler)
+
+  rotation = JSON.parse(HTTP::Client.post("https://api.mymicds.net/portal/day-rotation").body)
+  err = rotation["error"].as_s?
+
+  days = rotation["days"].as_h?
+
+  unless days.nil?
+    days = days.each_with_object({} of Time => Int32) do |(year, v0), memo|
+      v0.as(Hash(String, JSON::Type)).each do |(month, v1)|
+        v1.as(Hash(String, JSON::Type)).each do |(day, v2)|
+          cycle_day = JSON::Any.new(v2).as_i
+          memo[Time.new(year.to_i, month.to_i, day.to_i)] = cycle_day unless cycle_day == 1 || cycle_day == 4
+        end
+      end
+    end
+
+    day_pairs = advisees.cycle.zip(days.each)
+  end
+
+  partial :advisory, "Advisory Snack"
+end
 
 Kemal.run 3692
