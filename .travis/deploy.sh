e2b18d0f4854639e9cb9ce2ee8311a9f9af2ca6d
Nick Clifford <nick@nickclifford.me>
2018-03-13 18:25:33 -0500

    update deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,6 +10,7 @@ git push deploy master
 
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
+  shards
   crystal build --release --no-debug index.cr -o index.new
   mv index.new index # Do not trigger "index" file watching until compilation is done
 EOF
