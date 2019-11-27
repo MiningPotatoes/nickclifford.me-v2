2dc04873d50dead7b3fc7cad21521cb47aabe3c1
Nick Clifford <nick@nickclifford.me>
2019-11-26 22:48:47 -0600

    cleanup, move deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,12 +4,9 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 ssh-add .travis/id_rsa
 
-git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-git push deploy master
-
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
+  git pull
   shards install
   crystal build --release --no-debug index.cr -o index.new
   mv index.new index # Do not trigger "index" file watching until compilation is done
--- index.cr
+++ index.cr
@@ -1,8 +1,6 @@
 require "http/client"
-require "json"
 require "kemal"
 require "kilt/slang"
-require "sqlite3"
 require "yaml"
 require "./handlers/sass_handler"
 
@@ -20,19 +18,11 @@ macro partial(id, title)
   title = {{title}}
   stylesheet = %{<link href="/css/{{id.id}}.css" rel="stylesheet">} if File.exists?("./scss/{{id.id}}.scss")
 
-  # TODO: fix Slang to allow unescaped interpolation
   render "./views/{{id.id}}.slang", "./views/layout.slang"
 end
 
 get "/" do
   partial :index, "Home"
-end
-
-get "/cycling" do
-  DB.open "sqlite3://./misc.db" do |db|
-    cyclists = db.query_all("select name, falls from cycling order by falls desc", as: {name: String, falls: Int32})
-    partial :cycling, "Cycling"
-  end
 end
 
 Kemal.run 3692
